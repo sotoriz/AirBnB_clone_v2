2f73cc07bde101f1fd90ff4fb44b3085aa80ec7d
Sotonye Orifama <sotorisfx@gmail.com>
2023-04-06 01:31:58 -0700

    Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,7 +6,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['54.237.3.177', '54.237.31.150']
 
 
 def do_pack():
