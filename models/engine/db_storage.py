829f3b3168cc146fd2ac6b5c628fec359a51aa6e
sotoriz <sotorisfx@gmail.com>
2023-04-23 15:27:42 -0700

    Improve engines

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''database storage engine'''
-
+Attributes
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from models.amenity import Amenity
